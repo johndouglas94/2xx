a59c47d15df0ce04be143875d1a0187e7dc0cca9
John  Douglas <nightwolfgaming@Johns-MacBook-Pro.local>
2018-11-11 21:13:45 -0500

    added copyright and versioning

--- include/footer.inc.php
+++ include/footer.inc.php
@@ -3,6 +3,8 @@
 			<h4>this is the footer</h4>
 			<p>we place redundant information here</p>
 			<div id="footerTagLine">I am 7 inches left of normal</div>
+		    <div class="copyright">iop project - Version <?php echo VERSION; ?> | &copy; 1994 - <?php echo date('Y'); ?> John Douglas</div>
+
 		</footer>
 	</body>
 	
