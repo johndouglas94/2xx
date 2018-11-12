448fefc7f18f0f465c829a6ab4b019f6357f6fe0
John  Douglas <nightwolfgaming@Johns-MacBook-Pro.local>
2018-11-11 22:39:04 -0500

    updates to functions and index.php

--- include/functions.inc.php
+++ include/functions.inc.php
@@ -4,7 +4,7 @@
 	function menuBuilder( $obj ) {
      $menRtn = '<ul>';
      foreach ( $obj as $key => $value ) {
-        if(basename($value[ 'pageLink' ],'.html') != 'home'){
+        if(basename($value[ 'pageLink' ],'.html') != 'home') {
             $menRtn .= '<li>';
             $menRtn .= '<a href="index.php?pg=' . basename($value[ 'pageLink' ],'.html') . '">' . $value[ 'pageName' ] . '</a>';
             if ( is_array( $value[ 'childPages' ] ) ) {
@@ -16,6 +16,20 @@
      $menRtn .= '</ul>';
      return $menRtn;
 }
+
+
+function getPage( $obj, $pg = DEFUALT_PAGE ) {
+
+                         $pg = ( !empty( $_GET[ 'pg' ] ) ) ? $_GET[ 'pg' ] : $pg;
+
+                         foreach ( $obj as $name => $value ) {
+                              if ( basename($value[ 'pageLink' ],'.html') == $pg ) {
+                                   echo '<div class="container animated fadeIn"><h1>' . $value[ 'pageName' ] .'</h1></div><div class="container animated fadeIn">' . $value[ 'pageContent' ] . '</div>';
+                              } elseif ( is_array( $value[ 'childPages' ] )) {
+                                   getPage( $value[ 'childPages' ], $pg );
+                              }
+                         }
+                    }
 
 
 ?>
--- index.php
+++ index.php
@@ -2,12 +2,9 @@
 
 	require_once('include/header.inc.php'); 
 
-?>	
-		<section>
-			<p>this is where the page content goes</p>
-		</section>
+	
+	 getPage($content['pages']);
 
-<?php
 
 	require_once('include/footer.inc.php'); 
 
